47b3e1699f9ab5bc5cdb1d16a6b23e3331f622e2
Harman Waheed <139279892+harmanpythanist@users.noreply.github.com>
2026-06-07 08:15:21 +0500

    Add files via upload

---
+++ bookkepping_project/starting_again/function1 part1.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy as np
+from datetime import datetime
+#import pdfplumber
+
+# Create a function that reads all files (csv, excel, pdf) from a folder automatically
+
+#==========================================
+# CALLING FUNCTIONS LATER:
+#import_bank_transactions("file1", "csv")
+#import_bank_transactions("file2", "excel")
+#import_bank_transactions("file3", "pdf")
+#==========================================
+
+def import_bank_transactions(file_path, file_type='csv'):
+    """
+    Import transactions from various file formats
+    """
+    if file_type == 'csv':
+        df = pd.read_csv(file_path)
+    elif file_type == 'excel':
+        df = pd.read_excel(file_path)
+    elif file_type == 'pdf':
+        df = extract_from_pdf(file_path)   # TO be done!
+        
+    return df
+
+
+file = import_bank_transactions("D://test.csv", "excel")
+print(file)
+
+
+
+
+
